6de73cbba4462249b097f454f689ffcf0678b9b6
Cauê Gomes <caue.gprieto@gmail.com>
2026-02-27 19:18:33 -0400

    feat: initial commit

---
+++ src/main/java/dev/caue/smartfinance/SmartfinanceApplication.java
@@ -0,0 +1,11 @@
+package dev.caue.smartfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartfinanceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmartfinanceApplication.class, args);
+    }
+}
