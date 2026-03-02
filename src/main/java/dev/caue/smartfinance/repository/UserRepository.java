f2c288df8cd399da70e2bf1ad7f8654258af305a
Cauê Gomes <caue.gprieto@gmail.com>
2026-03-01 23:27:03 -0400

    feat: add User and Role entities with repository interface

---
+++ src/main/java/dev/caue/smartfinance/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package dev.caue.smartfinance.repository;
+
+import dev.caue.smartfinance.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
