2b61b40650b965bcc800006fc84b66b606ffb215
Cauê Gomes <caue.gprieto@gmail.com>
2026-03-03 22:31:29 -0400

    feat: implement UserDetailsImpl for Spring Security user details

---
+++ src/main/java/dev/caue/smartfinance/service/UserDetailsImpl.java
@@ -0,0 +1,34 @@
+package dev.caue.smartfinance.service;
+
+import dev.caue.smartfinance.domain.User;
+import lombok.Getter;
+import lombok.NonNull;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+public class UserDetailsImpl implements UserDetails {
+
+    private final Long id;
+    private final String email;
+    private final String password;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    public UserDetailsImpl(User user) {
+        this.id = user.getId();
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+        this.authorities =
+                List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+    }
+
+    @Override
+    @NonNull
+    public String getUsername() {
+        return email;
+    }
+}
