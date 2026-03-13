0afdc1fcfad18d0814da20cbff8719cfdc9a7ce1
Cauê Gomes <caue.gprieto@gmail.com>
2026-03-13 00:10:03 -0400

    refactor: remove unused import for Category in TransactionRequest

--- src/main/java/dev/caue/smartfinance/dto/TransactionRequest.java
+++ src/main/java/dev/caue/smartfinance/dto/TransactionRequest.java
@@ -1,6 +1,5 @@
 package dev.caue.smartfinance.dto;
 
-import dev.caue.smartfinance.domain.category.Category;
 import dev.caue.smartfinance.domain.transaction.TransactionType;
 
 import jakarta.validation.constraints.NotBlank;
